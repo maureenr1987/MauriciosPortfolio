bf4d5e6e3a64c0ff4b342f58c26df9b2c8fd7210
maureenr1987 <maureenr1987@gmail.com>
2019-01-24 23:27:48 -0500

    Better carousel, other stuff

--- js/Gallery.js
+++ js/Gallery.js
@@ -10,15 +10,23 @@ var CurrentPic = 0;
 
 //Buffer with all pictures
 var PicBuffer = "";
+var DotBuffer = "";
 
 //Format divs and pics
 for (i = 0; i < Pictures.length; i++) {
-    PicBuffer += "<div class='col' id='picture_" + i + "'><img src='" + Pictures[i] + "' width='100%' alt=''></img></div>"
+    PicBuffer += "<img src='" + Pictures[i] + "' width='100%' alt='' id='picture_" + i + "'></img>";
+    DotBuffer += "<span class='dot' id='dot_" + i + "' onclick='SetPic(" + i + ")'></span>";
 }
 
 //Add new divs
 document.getElementById("picture_frame").innerHTML = PicBuffer;
+document.getElementById("dots").innerHTML = DotBuffer;
 
+
+function SetPic(Pic) {
+    CurrentPic = Pic;
+    RefreshPics();
+}
 
 //Set next pic
 function NextPic() {
@@ -41,15 +49,17 @@ function PrevPic() {
 //Hide unused pictures and show current pic
 function RefreshPics () {
     for (i = 0; i < Pictures.length; i++) {
-
         if ( i == CurrentPic ) {
         document.getElementById("picture_" + i).hidden = false;
+        document.getElementById("dot_" + i).style.backgroundColor = "#717171";
         }
-    
         else {
             document.getElementById("picture_" + i).hidden = true;
+            document.getElementById("dot_" + i).style.backgroundColor = "#bbb";
         }
     }
+    
+    document.getElementById("on").innerHTML = (CurrentPic + 1) + " / " + Pictures.length;
 }
 
 //Start
